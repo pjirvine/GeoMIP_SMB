618be63d1e17499b522b252f0e78ee857399eedc
Peter Irvine <p.j.irvine@gmail.com>
2018-06-01 11:12:55 -0400

    tidying up

--- project_env.sh
+++ project_env.sh
@@ -5,7 +5,7 @@
 
 # load project environment
 . ~/apps/bin/module_load_17_05_17
-# 10 hour interactive session
-srun -p test --tunnel=8200:8200 --pty --mem 3000 -t 0-08:00 /bin/bash
 # open jupyter notebook in projects folder
 cd ~/projects
+# 10 hour interactive session
+srun -p test --tunnel=8200:8200 --pty --mem 3000 -t 0-08:00 /bin/bash
