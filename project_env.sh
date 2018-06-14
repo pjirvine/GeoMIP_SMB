10a942e2dcb237a2ad40c233cefbce521e881c54
Peter Irvine <p.j.irvine@gmail.com>
2018-06-14 13:08:15 -0400

    updated project_env for new centos7 setup

--- project_env.sh
+++ project_env.sh
@@ -3,9 +3,11 @@
 # Sets up environment and starts standard interactive session
 ##################################################
 
+# source centos7 setup
+source centos7-modules.sh
 # load modules
-module load cdo
-module load python/2.7.13-fasrc01
+module load gcc/7.1.0-fasrc01 cdo/1.9.4-fasrc02
+module load python/2.7.14-fasrc01
 # load project environment
 source activate analysis_01_06_18
 
